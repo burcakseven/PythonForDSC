d1e420e93dde776814699cfd24b0b5d16bb766d4
Burcak Seven <buseven@k1m42s05.42istanbul.com.tr>
2024-01-11 19:07:58 +0300

    done

--- Starting/Exception/whatis.py
+++ Starting/Exception/whatis.py
@@ -1,10 +1,25 @@
 import sys
 
+def evenOddFunc(input):
+    
+    if(input % 2 == 0):
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
 try:
     arg = sys.argv
-    print(type(arg[1]))
-    if len(arg) != 2 or not str.isdecimal(arg[1]):
-        raise AssertionError("denem")
-    
+    if len(arg) != 2:
+        if len(arg) > 1:
+            raise AssertionError("more than one argument is provided")
+        else:
+            print("")
+    elif not arg[1].isdigit():
+        raise AssertionError("argument is not an integer")
+    else:
+        evenOddFunc(int(arg[1]))
+
 except AssertionError as a:
-    print(a)
+    print("AssertionError: " + str(a))
+    
+
