4758dcc50cb51f721293c347b8bf36dd2a2c8365
Burcak Seven <buseven@k1m32s03.42istanbul.com.tr>
2024-01-18 19:29:26 +0300

    done

--- Starting/tqdm/Loading.py
+++ Starting/tqdm/Loading.py
@@ -1,2 +1,21 @@
-def ft_tqdm():
-    pass
+from time import sleep
+
+def loading(now, space, percent):
+    str = f"{percent}%|"
+    str += chr(0x2588) * int((now/7*7))
+    str += chr(int(0x258F) - (now % 7)) if (now % 7) == 0 else ''
+    str += (' ' * space)
+    return str
+    
+def convert_percent(now, lnght):
+    full = 78
+    fill = int(now / lnght * full)
+    space = full - fill
+    percent = int(now / lnght * 100)
+    return fill, space , percent
+    
+def ft_tqdm(numbers : range):
+    for now in numbers:
+        fill, space, percent = convert_percent(now, numbers[-1])
+        print(loading(fill,space, percent)+ '|', end= '\r')
+        yield
--- Starting/tqdm/tester.py
+++ Starting/tqdm/tester.py
@@ -2,9 +2,9 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-for elem in ft_tqdm(range(333)):
-    sleep(0.005)
+for elem in ft_tqdm(range(50)):
+    sleep(0.05)
 print()
-for elem in tqdm(range(333)):
-    sleep(0.005)
-print()
+
+for elem in tqdm(range(50)):
+    sleep(0.05)
