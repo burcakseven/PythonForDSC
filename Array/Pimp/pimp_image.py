df9896884ade0b50c6629bcd0fa39878d551e359
burcakseven <burcakseven123@gmail.com>
2024-01-23 10:52:06 +0300

    invert happend

---
+++ Array/Pimp/pimp_image.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ft_invert(array) -> np.array:
+    new_image = 255 - array 
+    plt.imshow(new_image)
+    plt.show()
+    return new_image
+
+def ft_red(array) -> np.array:
+    pass
+
+def ft_green(array) -> np.array:
+    pass
+
+def ft_blue(array) -> np.array:
+    pass
+
+def ft_grey(array) -> np.array:
+    pass
