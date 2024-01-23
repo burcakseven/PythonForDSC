bbc67acebca84808cf9cb13b9739825dd59b1ef0
burcakseven <burcakseven123@gmail.com>
2024-01-23 11:52:39 +0300

    done

--- Array/Pimp/pimp_image.py
+++ Array/Pimp/pimp_image.py
@@ -3,19 +3,50 @@ import matplotlib.pyplot as plt
 
 
 def ft_invert(array) -> np.array:
+    '''
+    Inverts the color of the image received
+    '''
     new_image = 255 - array 
     plt.imshow(new_image)
     plt.show()
     return new_image
 
 def ft_red(array) -> np.array:
-    pass
+    '''
+    Returns the red color of the image received
+    '''
+    red_array = array.copy()
+    red_array[:, :, 1:] = 0
+    plt.imshow(red_array)
+    plt.show()
+    return red_array
 
 def ft_green(array) -> np.array:
-    pass
+    '''
+    Returns the green color of the image received
+    '''
+    green_array = array.copy()
+    green_array[:, :, [0,2]] = 0
+    plt.imshow(green_array)
+    plt.show()
+    return green_array
 
 def ft_blue(array) -> np.array:
-    pass
+    '''
+    Returns the blue color of the image received
+    '''
+    blue_array = array.copy()
+    blue_array[:, :, :2] = 0
+    plt.imshow(blue_array)
+    plt.show()
+    return blue_array
 
 def ft_grey(array) -> np.array:
-    pass
+    '''
+    Returns the grey color of the image received
+    '''
+    grey_array = array.copy()
+    grey_array = np.mean(array, axis=2)
+    plt.imshow(grey_array, cmap="gray")
+    plt.show()
+    return grey_array
